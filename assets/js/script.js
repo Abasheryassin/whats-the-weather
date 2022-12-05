43e678a23cf6f54d612f8aed2a912b0b5711811b
abasheryassin <abasheryassin@gmail.com>
2022-12-05 15:17:04 -0600

    retrived weather data from api and saved city search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,32 @@
+// my wather API code 4c3954b1395c7d9228bb76f474415aa0
 var saveBtn = $('.btn');
+var savedCities = [];
 
 var citySearch;
 
-
-//Fetch 5 day weather forcast api corrdinates
+//save searched input
+saveBtn.on("click", function () {
+    citySearch = $(this).prev().val();
+    getCityLocation();
+});
 
 //Fetch api coordinates
 
-//save searched input
-saveBtn.on("click", function() {
-    citySearch = $(this).prev().val();
-});
+function getCityLocation(response) {
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + citySearch + '&appid=4c3954b1395c7d9228bb76f474415aa0')
+        .then(function (response) {
+            if (response === 404) {
+                console.log('Enter a real city');
+            }
+            return response.json();
+        })
+        .then(function (data) {
+            savedCities += citySearch;
+            localStorage.setItem("cities", JSON.stringify(savedCities));
+            console.log(data);
+        })
+    }
+//Fetch 5 day weather forcast api corrdinates
 
 //display current weather
 
